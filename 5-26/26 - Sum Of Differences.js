0a95588382deac88decaa333804d2050f4576cb7
BoltenCoder <moltencreeper25@gmail.com>
2026-05-26 17:57:42 -0500

    5/26/26

---
+++ 5-26/26 - Sum Of Differences.js
@@ -0,0 +1,21 @@
+// Add all elements in an array of numbers.
+function sumOfArray(arr) {
+  return arr.reduce((acc, cur) => {
+    return acc + cur
+  })
+}
+
+function sumOfDifferences(arr) {
+  // Create a new array of just the differences in the numbers of the original array.
+  let toAdd = arr.reduce((acc, cur, i) => {
+    return arr[i + 1]
+    ? [...acc, (arr[i + 1] - cur)]
+    : acc
+  }, [])
+
+  const diffSum = sumOfArray(toAdd)
+  console.log(diffSum)
+  return diffSum;
+}
+
+sumOfDifferences([1, 3, 4])
