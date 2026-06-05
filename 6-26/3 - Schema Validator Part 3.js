e93224a2789abf128599ec3ead57dea7d30033f0
BoltenCoder <moltencreeper25@gmail.com>
2026-06-04 19:05:56 -0500

    6/3/26

---
+++ 6-26/3 - Schema Validator Part 3.js
@@ -0,0 +1,40 @@
+// Array of valid possible roles.
+const validRoles = ["user", "creator", "moderator", "staff", "admin"]
+
+// Object of all acceptable value types for each key.
+const validSchema = {
+  username: "string",
+  posts: "number",
+  verified: "boolean",
+  role: "string"
+}
+
+function isValidSchema(obj, schema = validSchema) {
+  
+  // Verify all necessary keys are a part of the object and automatically pass any extra keys given.
+  const givenKeys = Object.keys(obj) // Make an array of all keys in the passed object.
+   // Check each non-extra entry verifying it matches the schema.
+  const allKeysValid = givenKeys.every((key) => {
+    if (key in schema) {
+      return typeof(obj[key]) === schema[key]
+    } else {
+      return true
+    }
+  })
+
+  // Verify that the role value exists and is one of the allowed obtions from the "validRoles" variable.
+  let roleValid = false
+  if (obj["role"] && validRoles.includes(obj["role"])) {
+    roleValid = true
+  }
+
+  // If both checks pass, consider the object verified as valid overall
+  const verified = allKeysValid && roleValid
+    ? true
+    : false
+
+  console.log(verified)
+  return verified;
+}
+
+isValidSchema({ username: "sara", posts: 45, verified: false, role: "creator", followers: 70 })
